f3cbea22c20f5a5dd81788b97232bc201286d2b5
abdullmustyy <abdulmusty03@outlook.com>
2024-03-19 17:03:53 +0100

    feat: add schadcn and start the auth pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
+
 const App = () => {
-  return <h1 className="h1-bold underline">Hello fron SnapGram!</h1>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* Private Routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const SignupForm = () => {
+  return (
+    <div>
+      <Button variant={"destructive"}>Sign Up</Button>
+    </div>
+  );
+};
+
+export default SignupForm;
